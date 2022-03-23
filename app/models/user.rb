1fda4c51f849ba8a6753bef3be8076dab0d70928
max-las <lasserre.maxime@yahoo.fr>
2022-03-23 04:44:07 +0100

    crud users + add missing questions and super_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :organisation
+  belongs_to :organisation, optional: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_142211) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_23_033336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_142211) do
     t.integer "impact_estimation_realite"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "pouvoir_strategique_implication_partage"
+    t.float "pouvoir_gouvernance_taux_droits_vote_salaries"
     t.index ["organisation_id"], name: "index_evaluations_on_organisation_id"
   end
 
@@ -107,6 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_142211) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_super_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
