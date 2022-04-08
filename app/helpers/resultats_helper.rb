76b521c0e5c81ec3a076abd24d595f2cdb981b99
max-las <lasserre.maxime@yahoo.fr>
2022-04-08 09:38:06 +0200

    single value alternative

--- app/helpers/resultats_helper.rb
+++ app/helpers/resultats_helper.rb
@@ -19,4 +19,39 @@ module ResultatsHelper
       }
     }
   end
+
+  # def single_value_pie_chart(value, suffix: nil, max: nil)
+  #   suffix ||= "%"
+  #   max ||= 100
+
+  #   data = {
+  #     "value" => value,
+  #     "remainder" => (max - value)
+  #   }
+
+  #   chart = pie_chart data,
+  #     donut: true, suffix: "%", colors: ["#2E3092", "#ECD1D8"], 
+  #     library: { 
+  #       plugins: {
+  #         tooltip: {
+  #           filter: "__fn__tooltipFilter"
+  #         },
+  #         legend: {
+  #           labels: {
+  #             filter: "__fn__legendFilter"
+  #           }
+  #         }
+  #       }
+  #     }
+    
+  #   chart["\"__fn__tooltipFilter\""]= raw "function(a) {
+  #     return a.dataIndex === 0;
+  #   }"
+
+  #   chart["\"__fn__legendFilter\""]= raw "function(legendItem) {
+  #     return legendItem.index === 0;
+  #   }"
+
+  #   return chart
+  # end
 end
