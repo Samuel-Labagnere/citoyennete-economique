3382530d6c0be342f74e298f7d57312765f112f2
max-las <lasserre.maxime@yahoo.fr>
2022-04-13 19:38:11 +0200

    drop usage of @latest_states

--- app/controllers/statistiques_controller.rb
+++ app/controllers/statistiques_controller.rb
@@ -1,31 +1,24 @@
 class StatistiquesController < ApplicationController
   def pillar1
-    aggregate = Aggregate.new
-    @pouvoir_gouvernance_taux = aggregate.pouvoir_gouvernance_taux
+    @pouvoir_gouvernance_taux = Aggregate.pouvoir_gouvernance_taux
   end
 
   def pillar2
-    aggregate = Aggregate.new
-
-    @valeur_perennite_booleens = aggregate.valeur_perennite_booleens
-    @valeur_part_resultat = aggregate.valeur_part_resultat
-    @valeur_partage_booleens = aggregate.valeur_partage_booleens
+    @valeur_perennite_booleens = Aggregate.valeur_perennite_booleens
+    @valeur_part_resultat = Aggregate.valeur_part_resultat
+    @valeur_partage_booleens = Aggregate.valeur_partage_booleens
   end
 
   def pillar3
-    aggregate = Aggregate.new
-
-    @qualite_qvt_enquete_qvt = aggregate.qualite_qvt_enquete_qvt
-    @qualite_qvt_taux_all = aggregate.qualite_qvt_taux_all
-    @qualite_emancipation_taux = aggregate.qualite_emancipation_taux
+    @qualite_qvt_enquete_qvt = Aggregate.qualite_qvt_enquete_qvt
+    @qualite_qvt_taux_all = Aggregate.qualite_qvt_taux_all
+    @qualite_emancipation_taux = Aggregate.qualite_emancipation_taux
   end
 
   def pillar4
-    aggregate = Aggregate.new
-
-    @impact_environnement_booleens = aggregate.impact_environnement_booleens
-    @impact_environnement_scope_booleens = aggregate.impact_environnement_scope_booleens
-    @impact_engagement = aggregate.impact_engagement
-    @impact_engagement_demarche_ethique_fournisseurs = aggregate.impact_engagement_demarche_ethique_fournisseurs
+    @impact_environnement_booleens = Aggregate.impact_environnement_booleens
+    @impact_environnement_scope_booleens = Aggregate.impact_environnement_scope_booleens
+    @impact_engagement = Aggregate.impact_engagement
+    @impact_engagement_demarche_ethique_fournisseurs = Aggregate.impact_engagement_demarche_ethique_fournisseurs
   end
 end
--- app/models/aggregate.rb
+++ app/models/aggregate.rb
@@ -1,9 +1,106 @@
 class Aggregate
-  def indicateur_clean(symbol)
+
+  # PILLAR 1
+
+  def self.pouvoir_gouvernance_taux
+  {
+    indicateur_clean(:pouvoir_gouvernance_part_salaries_associes) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_part_salaries_associes),
+    indicateur_clean(:pouvoir_gouvernance_taux_societariat_femmes) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_taux_societariat_femmes),
+    indicateur_clean(:pouvoir_gouvernance_taux_droits_vote_salaries) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_taux_droits_vote_salaries),
+    indicateur_clean(:pouvoir_gouvernance_part_femmes_conseil) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_part_femmes_conseil)
+  }
+  end
+
+  # PILLAR 2
+
+  def self.valeur_part_resultat
+  {
+    indicateur_clean(:valeur_partage_part_resultat_net_actionnaires) => State.evaluations.up_to_date.median(:valeur_partage_part_resultat_net_actionnaires),
+    indicateur_clean(:valeur_partage_part_resultat_salaries) => State.evaluations.up_to_date.median(:valeur_partage_part_resultat_salaries)
+  }
+  end
+
+  def self.valeur_perennite_booleens
+    boolean_group(State.evaluations.up_to_date, [
+      :valeur_perennite_existence_reserve,
+      :valeur_perennite_reserve_impartageable,
+      :valeur_perennite_attention_questions_innovation
+    ])
+  end
+
+  def self.valeur_partage_booleens
+    boolean_group(State.evaluations.up_to_date, [
+      :valeur_partage_existence_accord,
+      :valeur_partage_existence_epargne_salariale,
+      :valeur_partage_existence_grille_salariale,
+      :valeur_partage_publicite_grille
+    ])
+  end
+
+  # PILLAR 3
+
+  def self.qualite_qvt_enquete_qvt
+    single_boolean(State.evaluations.up_to_date, :qualite_qvt_enquete_qvt)
+  end
+
+  def self.qualite_qvt_taux_all
+  {
+    indicateur_clean(:qualite_qvt_taux_qvt) => State.evaluations.up_to_date.median(:qualite_qvt_taux_qvt),
+    indicateur_clean(:qualite_qvt_part_cdi) => State.evaluations.up_to_date.median(:qualite_qvt_part_cdi),
+    indicateur_clean(:qualite_qvt_taux_turnover) => State.evaluations.up_to_date.median(:qualite_qvt_taux_turnover),
+    indicateur_clean(:qualite_qvt_taux_absenteisme) => State.evaluations.up_to_date.median(:qualite_qvt_taux_absenteisme)
+  }
+  end
+
+  def self.qualite_emancipation_taux
+  {
+    indicateur_clean(:qualite_emancipation_taux_budget_formation_masse_salariale) => State.evaluations.up_to_date.median(:qualite_emancipation_taux_budget_formation_masse_salariale),
+    indicateur_clean(:qualite_emancipation_taux_direction_promotion_interne) => State.evaluations.up_to_date.median(:qualite_emancipation_taux_direction_promotion_interne)
+  }
+  end
+
+  # PILLAR 4
+
+  def self.impact_environnement_booleens
+    boolean_group(State.evaluations.up_to_date, [
+      :impact_environnement_audit_impact,
+      :impact_environnement_demarche_ecologique,
+      :impact_environnement_discussion_impact
+    ])
+  end
+
+  def self.impact_environnement_scope_booleens
+    scope1 = State.evaluations.up_to_date.where(impact_environnement_bilan_carbone_scope_1: true).count
+    scope2 = State.evaluations.up_to_date.where(impact_environnement_bilan_carbone_scope_2: true).count
+    scope3 = State.evaluations.up_to_date.where(impact_environnement_bilan_carbone_scope_3: true).count
+
+    {
+      indicateur_clean(:impact_environnement_bilan_carbone_scope_1) => (scope1.to_f / State.evaluations.up_to_date.count * 100),
+      indicateur_clean(:impact_environnement_bilan_carbone_scope_2) => (scope2.to_f / State.evaluations.up_to_date.count * 100),
+      indicateur_clean(:impact_environnement_bilan_carbone_scope_3) => (scope3.to_f / State.evaluations.up_to_date.count * 100),
+      'Total' => ((scope1 + scope2 + scope3).to_f / State.evaluations.up_to_date.count * 100)
+    }
+  end
+
+  def self.impact_engagement
+  {
+    indicateur_clean(:impact_engagement_part_resultat_missions_utiles) => State.evaluations.up_to_date.median(:impact_engagement_part_resultat_missions_utiles),
+    indicateur_clean(:impact_engagement_part_placements_responsables) => State.evaluations.up_to_date.median(:impact_engagement_part_placements_responsables),
+    indicateur_clean(:impact_engagement_part_impots_france) => State.evaluations.up_to_date.median(:impact_engagement_part_impots_france)
+  }
+  end
+
+  def self.impact_engagement_demarche_ethique_fournisseurs
+    single_boolean(State.evaluations.up_to_date, :impact_engagement_demarche_ethique_fournisseurs)
+  end
+
+  protected
+
+  def self.indicateur_clean(symbol)
     I18n.t("activerecord.attributes.state." + symbol.to_s)
   end
 
-  def boolean_group(states, indic_symbols)
+  def self.boolean_group(states, indic_symbols)
     group = {}
 
     indic_symbols.each do |indic_symbol|
@@ -16,7 +113,7 @@ class Aggregate
     return group
   end
 
-  def single_boolean(states, indic_symbol)
+  def self.single_boolean(states, indic_symbol)
     result = nil
 
     true_count = states.where("#{indic_symbol.to_s} = ?", true).count
@@ -31,99 +128,5 @@ class Aggregate
     end
 
     return result
-  end
-
-  # PILLAR 1
-
-  def pouvoir_gouvernance_taux
-  {
-    indicateur_clean(:pouvoir_gouvernance_part_salaries_associes) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_part_salaries_associes),
-    indicateur_clean(:pouvoir_gouvernance_taux_societariat_femmes) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_taux_societariat_femmes),
-    indicateur_clean(:pouvoir_gouvernance_taux_droits_vote_salaries) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_taux_droits_vote_salaries),
-    indicateur_clean(:pouvoir_gouvernance_part_femmes_conseil) => State.evaluations.up_to_date.median(:pouvoir_gouvernance_part_femmes_conseil)
-  }
-  end
-
-  # PILLAR 2
-
-  def valeur_part_resultat
-  {
-    indicateur_clean(:valeur_partage_part_resultat_net_actionnaires) => State.evaluations.up_to_date.median(:valeur_partage_part_resultat_net_actionnaires),
-    indicateur_clean(:valeur_partage_part_resultat_salaries) => State.evaluations.up_to_date.median(:valeur_partage_part_resultat_salaries)
-  }
-  end
-
-  def valeur_perennite_booleens
-    boolean_group(State.evaluations.up_to_date, [
-      :valeur_perennite_existence_reserve,
-      :valeur_perennite_reserve_impartageable,
-      :valeur_perennite_attention_questions_innovation
-    ])
-  end
-
-  def valeur_partage_booleens
-    boolean_group(State.evaluations.up_to_date, [
-      :valeur_partage_existence_accord,
-      :valeur_partage_existence_epargne_salariale,
-      :valeur_partage_existence_grille_salariale,
-      :valeur_partage_publicite_grille
-    ])
-  end
-
-  # PILLAR 3
-
-  def qualite_qvt_enquete_qvt
-    single_boolean(State.evaluations.up_to_date, :qualite_qvt_enquete_qvt)
-  end
-
-  def qualite_qvt_taux_all
-  {
-    indicateur_clean(:qualite_qvt_taux_qvt) => State.evaluations.up_to_date.median(:qualite_qvt_taux_qvt),
-    indicateur_clean(:qualite_qvt_part_cdi) => State.evaluations.up_to_date.median(:qualite_qvt_part_cdi),
-    indicateur_clean(:qualite_qvt_taux_turnover) => State.evaluations.up_to_date.median(:qualite_qvt_taux_turnover),
-    indicateur_clean(:qualite_qvt_taux_absenteisme) => State.evaluations.up_to_date.median(:qualite_qvt_taux_absenteisme)
-  }
-  end
-
-  def qualite_emancipation_taux
-  {
-    indicateur_clean(:qualite_emancipation_taux_budget_formation_masse_salariale) => State.evaluations.up_to_date.median(:qualite_emancipation_taux_budget_formation_masse_salariale),
-    indicateur_clean(:qualite_emancipation_taux_direction_promotion_interne) => State.evaluations.up_to_date.median(:qualite_emancipation_taux_direction_promotion_interne)
-  }
-  end
-
-  # PILLAR 4
-
-  def impact_environnement_booleens
-    boolean_group(State.evaluations.up_to_date, [
-      :impact_environnement_audit_impact,
-      :impact_environnement_demarche_ecologique,
-      :impact_environnement_discussion_impact
-    ])
-  end
-
-  def impact_environnement_scope_booleens
-    scope1 = State.evaluations.up_to_date.where(impact_environnement_bilan_carbone_scope_1: true).count
-    scope2 = State.evaluations.up_to_date.where(impact_environnement_bilan_carbone_scope_2: true).count
-    scope3 = State.evaluations.up_to_date.where(impact_environnement_bilan_carbone_scope_3: true).count
-
-    {
-      indicateur_clean(:impact_environnement_bilan_carbone_scope_1) => (scope1.to_f / State.evaluations.up_to_date.count * 100),
-      indicateur_clean(:impact_environnement_bilan_carbone_scope_2) => (scope2.to_f / State.evaluations.up_to_date.count * 100),
-      indicateur_clean(:impact_environnement_bilan_carbone_scope_3) => (scope3.to_f / State.evaluations.up_to_date.count * 100),
-      'Total' => ((scope1 + scope2 + scope3).to_f / State.evaluations.up_to_date.count * 100)
-    }
-  end
-
-  def impact_engagement
-  {
-    indicateur_clean(:impact_engagement_part_resultat_missions_utiles) => State.evaluations.up_to_date.median(:impact_engagement_part_resultat_missions_utiles),
-    indicateur_clean(:impact_engagement_part_placements_responsables) => State.evaluations.up_to_date.median(:impact_engagement_part_placements_responsables),
-    indicateur_clean(:impact_engagement_part_impots_france) => State.evaluations.up_to_date.median(:impact_engagement_part_impots_france)
-  }
-  end
-
-  def impact_engagement_demarche_ethique_fournisseurs
-    single_boolean(State.evaluations.up_to_date, :impact_engagement_demarche_ethique_fournisseurs)
   end
 end
