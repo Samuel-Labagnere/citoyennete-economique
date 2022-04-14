f232c4933cfa4598655ccd25d07b5f8eb7300999
max-las <lasserre.maxime@yahoo.fr>
2022-04-14 12:47:30 +0200

    begin resultats

--- app/controllers/resultats_controller.rb
+++ app/controllers/resultats_controller.rb
@@ -1,16 +1,8 @@
 class ResultatsController < ApplicationController
-  def indicateurs_hash(evaluation, sym_list)
-    hash = {}
-    sym_list.each do |sym|
-      if evaluation.state[sym].present?
-        hash[helpers.state(sym)] = evaluation.state[sym]
-      end
-    end
-    return hash
-  end
-
   def index
-    @evaluation = Evaluation.find(params[:evaluation_id])
+    @evaluation = Evaluation.find(params[:evaluation_id]) # À supprimer
     @previous_evaluation = Evaluation.where(annee: ..(@evaluation.annee-1)).order("annee DESC").first
+
+    @state = Evaluation.find(params[:evaluation_id]).state
   end
 end
